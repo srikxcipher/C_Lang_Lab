6ce682fe99568806a78cc7ec809692653618e80a
Srikant Snehit Panda <163234695+srikxcipher@users.noreply.github.com>
2025-01-08 19:44:37 +0530

    Create shmwriter.c

---
+++ ass11/shmwriter.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+
+int main(){
+  int id, *p;
+  struct shmid_ds S;
+key_t key;
+key = ftok("p.c",500);
+printf("Key=%#X\n",key);
+id=shmget(key,sizeof(int),0660);
+printf("shm id=%d\n",id);
+p=(int *)shmat(id,NULL,0);
+printf("%d\n",*p);
+return 0;
+}
